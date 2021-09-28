89bb0c874b43c7b0c937bd63dfb75efa6cc1e4b0
Noah Wilder <noahleviwilder@gmail.com>
2021-09-28 15:44:34 -0400

    Created keyPath ForEach overloads with divider modifier closures.

--- Sources/SwiftUIUtilities/SwiftUIUtilities.swift
+++ Sources/SwiftUIUtilities/SwiftUIUtilities.swift
@@ -1,10 +1,6 @@
 #if canImport(SwiftUI)
 import SwiftUI
 
-
-
-
-
 extension ForEach where Content : View {
     public init<D, T, U>(
         _ data: D,
@@ -28,8 +24,30 @@ extension ForEach where Content : View {
         }
     }
     
+    public init<D, T, U>(
+        _ data: D,
+        id: KeyPath<D.Element, ID>,
+        @ViewBuilder content: @escaping (D.Element) -> T,
+        @ViewBuilder divider: @escaping (T) -> U
+    ) where
+        T: View,
+        U: View,
+        Content == SwiftUI._ConditionalContent<U, T>,
+        D: RandomAccessCollection,
+        Data == LazyMapSequence<D.Indices, (D.Index, ID)>
+    {
+        self.init(data.indices.lazy.map({ ($0, data[$0][keyPath: id]) }), id: \.1) { (index, _) in
+            if data.startIndex != data.endIndex, index < data.index(before: data.endIndex) {
+                divider(content(data[index]))
+            } else {
+                content(data[index])
+            }
+        }
+    }
+
     
 }
+
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 extension ForEach where Content: View {
     init<C, T, U>(
@@ -37,15 +55,15 @@ extension ForEach where Content: View {
         @ViewBuilder content: @escaping (Binding<C.Element>) -> T,
         @ViewBuilder divider: @escaping (T) -> U
     ) where
-    T: View,
-    U: View,
-    Content == SwiftUI._ConditionalContent<U, T>,
-    Data == LazyMapSequence<C.Indices, (C.Index, ID)>,
-    ID == C.Element.ID,
-    C: MutableCollection,
-    C: RandomAccessCollection,
-    C.Element : Identifiable,
-    C.Index : Hashable
+        T: View,
+        U: View,
+        Content == SwiftUI._ConditionalContent<U, T>,
+        Data == LazyMapSequence<C.Indices, (C.Index, ID)>,
+        ID == C.Element.ID,
+        C: MutableCollection,
+        C: RandomAccessCollection,
+        C.Element : Identifiable,
+        C.Index : Hashable
     {
         
         self.init(data.indices.lazy.map({ ($0, data[$0].id) }), id: \.1) { (index, _) in
@@ -57,10 +75,30 @@ extension ForEach where Content: View {
         }
     }
     
-    
+    init<C, T, U>(
+        _ data: Binding<C>,
+        id: KeyPath<C.Element, ID>,
+        content: @escaping (Binding<C.Element>) -> T,
+        divider: @escaping (T) -> U
+    ) where
+        T: View,
+        U: View,
+        Content == SwiftUI._ConditionalContent<U, T>,
+        Data == LazyMapSequence<C.Indices, (C.Index, ID)>,
+        C: MutableCollection,
+        C: RandomAccessCollection,
+        C.Index : Hashable
+    {
+        self.init(data.indices.lazy.map({ ($0, data[$0].wrappedValue[keyPath: id]) }), id: \.1) { (index, _) in
+            if data.startIndex != data.endIndex, index < data.index(before: data.endIndex) {
+                divider(content(data[index].projectedValue))
+            } else {
+                content(data[index].projectedValue)
+            }
+        }
+    }
 
 }
-
 
 extension ForEach where Data == Range<Int>, ID == Int, Content : View {
 
@@ -83,11 +121,6 @@ extension ForEach where Data == Range<Int>, ID == Int, Content : View {
     }
 }
 
-extension Int: Identifiable {
-    public var id: Int {
-        return self
-    }
-}
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 public struct Foo: View {
     
@@ -127,7 +160,7 @@ public struct Bar: View {
     @Binding var arr: [String]
     
     public var body: some View {
-        ForEach($arr) { str in
+        ForEach($arr, id: \.self) { str in
             TextField("text field", text: str)
         } divider: { view in
             VStack(spacing: 0) {
